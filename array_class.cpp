f6bea6e233d22982b5a8ae6205fb97130bafda19
gpx <1457495424@qq.com>
2019-01-03 18:55:31 +0800

    modify array_class.cpp

--- array_class.cpp
+++ array_class.cpp
@@ -1,9 +1,9 @@
 /*************************************************************************
-	> File Name: array_class.cpp
-	> Author: gpx
-	> Mail: 1457495424@qq.com
-	> Created Time: 2019年01月01日 星期二 20时16分13秒
- ************************************************************************/
+    > File Name: array_class.cpp
+    > Author: gpx
+    > Mail: 1457495424@qq.com
+    > Created Time: 2019年01月01日 星期二 20时16分13秒
+************************************************************************/
 
 #include <iostream>
 
@@ -12,37 +12,40 @@ using std::cout;
 using std::endl;
 
 class Array {
-    private:
-        int *data;
-        int length;
-    public:
-        Array() {
-            length = 10;
-            data = (int *)malloc(sizeof(int) * length);
+private:
+    int *data;
+    int length;
+public:
+    Array() {
+        length = 10;
+        data = new int[length];
+    }
+    Array(int len) {
+        length = len;
+        data = new int[length];
+    }
+    int getLength() {
+        return length;
+    }
+    int getValue(int ind) {
+        if (ind >= length || ind < 0) {
+            cout << "getValue segment fault ";
+            return -1;
         }
-        Array(int len) {
-            length = len;
-            data = (int *)malloc(sizeof(int) * length);
+        return data[ind];
+    }
+    bool setValue(int ind, int value) {
+        if (ind >= length || ind < 0) {
+            cout << "setValue " << ind << " segment faule " << endl;
+            return false;
         }
-        int getLength() {
-            return length;
-        }
-        int getValue(int ind) {
-            if (ind >= length || ind < 0) {
-                cout << "getValue segment fault ";
-                return -1;
-            }
-            return data[ind];
-        }
-        bool setValue(int ind, int value) {
-            if (ind >= length || ind < 0) {
-                cout << "setValue " << ind << " segment faule " << endl;
-                return false;
-            }
-            data[ind] = value;
-            cout << "setValue " << ind << " success!" << endl;
-            return true;
-        }
+        data[ind] = value;
+        cout << "setValue " << ind << " success!" << endl;
+        return true;
+    }
+    ~Array() {
+        delete []data;
+    }
 } ;
 
 int main() {
@@ -51,7 +54,7 @@ int main() {
     cout << "arr.length : " << arr.getLength() << endl;
     cout << "arr.data[2] : " << arr.getValue(2) << endl;
     cout << "arr.data[10] : " << arr.getValue(15) << endl;
-    
+
     cout << "arr1.length : " << arr1.getLength() << endl;
     arr1.setValue(5, 15);
     arr1.setValue(21, 10);
